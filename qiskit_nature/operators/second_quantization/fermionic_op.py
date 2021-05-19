58d50ba33095bdf58a266a8ec75a954db3e1ce9c
ikkoham <ikkoham@users.noreply.github.com>
2021-05-19 21:00:24 +0900

    Add SparseFermionicOp

--- qiskit_nature/operators/second_quantization/fermionic_op.py
+++ qiskit_nature/operators/second_quantization/fermionic_op.py
@@ -19,6 +19,7 @@ import numpy as np
 from qiskit.utils.validation import validate_min, validate_range_exclusive_max
 
 from qiskit_nature import QiskitNatureError
+
 from .second_quantized_op import SecondQuantizedOp
 
 
